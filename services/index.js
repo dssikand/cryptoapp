2d5d4a412cd527f3b4677df8ef9e1ade02d5749b
Akshay <Akshay Sharma>
2025-03-20 12:28:36 +0530

    change in app.jsx

--- services/index.js
+++ services/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Base_URL = axios.create({
-  // baseURL: 'https://www.qoyn.network/api/',
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://www.qoyn.network/api/',
+  // baseURL: 'http://localhost:3000/api/',
 });
 
 export default Base_URL;
